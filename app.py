87d4e8527200e30be3244a6d5a16249b3867eb0f
Shane Checkley <shane@Shanes-MacBook-Air.local>
2022-01-12 00:29:18 -0500

    made the get_input function in app.py file shorter and easier to read by creating helper functions. completed the basic menu and tested all user inputs

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ Please choose one if these options:
 
 1) Add a new transaction
 2) See all transactions
-3) Find a transactio by name(description)
+3) Find a transaction by name(description)
 4) Find total $ of category
 5) Exit
 
@@ -26,24 +26,40 @@ def get_input(connection):
     # ":=" gives a new variable (left side) a value (right side) within a statement
     while (user_input := input(MENU_PROMPT)) != "5":
         if user_input == "1":
-            date = input("Enter date: ")
-            description = input("Enter description: ")
-            transaction_type = input("Enter transaction type (withdrawal, deposit, transfer): ")
-            # casting input to int... look into this more
-            amount = int(input("Enter amount: "))
-            debit_or_credit = input("Debit or credit?: ")
-            purchase_method = input("Enter purchase method: ")
-            category = input("Enter category: ")
-
-            database.add_transaction(connection, date, description, transaction_type, amount, debit_or_credit, purchase_method, category)
+            get_new_transactions(connection)
         elif user_input == "2":
-            transactions = database.get_all_transactions(connection)
-
-            for transaction in transactions:
-                print(transaction)
+            see_all_transactions(connection)
         elif user_input == "3":
-            pass
+            find_by_description(connection)
         elif user_input == "4":
-            pass
+            sum_of_category(connection)
         else:
             print("****Invalid input, please try again!*****")
+
+def get_new_transactions(connection):
+    date = input("Enter date: ")
+    description = input("Enter description: ")
+    transaction_type = input("Enter transaction type (withdrawal, deposit, transfer): ")
+    # casting input to int... look into this more
+    amount = int(input("Enter amount: "))
+    debit_or_credit = input("Debit or credit?: ")
+    purchase_method = input("Enter purchase method: ")
+    category = input("Enter category: ")
+
+    database.add_transaction(connection, date, description, transaction_type, amount, debit_or_credit, purchase_method, category)
+
+def see_all_transactions(connection):
+    transactions = database.get_all_transactions(connection)
+
+    for transaction in transactions:
+        print(transaction)
+
+def find_by_description(connecition):
+    user_input = input("Enter description: ")
+    description = database.get_transactions_by_description(connecition, user_input)
+    print(description)
+
+def sum_of_category(connection):
+    category = input("Enter category: ")
+    amount = database.get_category_total(connection, category)
+    print(amount)
--- database.py
+++ database.py
@@ -1,42 +1,38 @@
 import sqlite3
 
 # SQL Queries
-CREATE_TRANSACTION_TABLE = "CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY, date TEXT, description TEXT, transaction_type TEXT, amount INTEGER, debit_or_credit TEXT, purchase_method TEXT, category TEXT);"
-INSERT_TRANSACTION = "INSERT INTO transactions (date, description, transaction_type, amount, debit_or_credit, purchase_method, category) VALUES (?, ?, ?, ?, ?, ?, ?);"
-GET_ALL_TRANSACTIONS = "SELECT * FROM transactions;"
-GET_TRANSACTIONS_BY_DESCRIPTION = "SELECT * FROM transactions WHERE description = ?;"
-GET_CATEGORY_TOTAL = """
-SELECT SUM(category) 
-FROM transactions
-WHERE category = ?
-"""
+CREATE_TABLE = "CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY, date TEXT, description TEXT, transaction_type TEXT, amount INTEGER, debit_or_credit TEXT, purchase_method TEXT, category TEXT);"
+INSERT = "INSERT INTO transactions (date, description, transaction_type, amount, debit_or_credit, purchase_method, category) VALUES (?, ?, ?, ?, ?, ?, ?);"
+GET_ALL = "SELECT * FROM transactions;"
+GET_BY_DESCRIPTION = "SELECT * FROM transactions WHERE description = ?;"
+GET_CATEGORY_SUM = "SELECT SUM(amount) FROM transactions WHERE category = ?"
 
-# connect to the data file
+# connect database to the data file
 def connect():
     return sqlite3.connect("data.db")
 
 def create_tables(connection):
     with connection:
-        connection.execute(CREATE_TRANSACTION_TABLE)
+        connection.execute(CREATE_TABLE)
 
 def add_transaction(connection, date, description, transaction_type, amount, debit_or_credit, purchase_method, category):
     with connection:
         # execution that requires values: values must be tuples
-        connection.execute(INSERT_TRANSACTION, (date, description, transaction_type, amount, debit_or_credit, purchase_method, category))
+        connection.execute(INSERT, (date, description, transaction_type, amount, debit_or_credit, purchase_method, category))
 
 def check_if_added(connection):
     pass
 
 def get_all_transactions(connection):
     with connection:
-        return connection.execute(GET_ALL_TRANSACTIONS).fetchall()
+        return connection.execute(GET_ALL).fetchall()
 
 def get_transactions_by_description(connection, description):
     with connection:
         # execution that requires values: values must be tuples
-        return connection.execute(GET_TRANSACTIONS_BY_DESCRIPTION, (description,))
+        return connection.execute(GET_BY_DESCRIPTION, (description,)).fetchone()
 
 def get_category_total(connection, category):
     with connection:
         # execution that requires values: values must be tuples
-        return connection.execute(GET_CATEGORY_TOTAL, (category,))
+        return connection.execute(GET_CATEGORY_SUM, (category,)).fetchone()
